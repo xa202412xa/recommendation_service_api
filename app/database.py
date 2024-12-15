0277403b909a5e3699985450c561dac3e0cb39cd
xa202412xa <xa202212xa@gmail.com>
2024-12-15 18:40:52 +0300

    Create database.py

---
+++ app/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DATABASE_URL = "postgresql+asyncpg://user:password@localhost/dbname"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+Base = declarative_base()
